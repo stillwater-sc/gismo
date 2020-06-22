34119049573e148eb45a17682ec8777647771c84
Hugo Verhelst <hugoverhelst1995@gmail.com>
2020-06-22 17:30:58 +0200

    backbone implementation of D-patch

--- examples/gsDPatch_example.cpp
+++ examples/gsDPatch_example.cpp
@@ -17,13 +17,12 @@
 
 using namespace gismo;
 
-
-
 int main(int argc, char* argv[])
 {
     bool plot = false;
     index_t numRefine  = 0;
     index_t numElevate = 0;
+    index_t geometry = 0;
     std::string input("surfaces/simple.xml");
 
     gsCmdLine cmd("Tutorial on gsGeometry class.");
@@ -31,6 +30,7 @@ int main(int argc, char* argv[])
     cmd.addInt( "e", "degreeElevation",
                 "Number of degree elevation steps to perform before solving (0: equalize degree in all directions)", numElevate );
     cmd.addInt( "r", "uniformRefine", "Number of Uniform h-refinement steps to perform before solving",  numRefine );
+    cmd.addInt( "g", "geometry", "which geometry",  geometry );
     cmd.addSwitch("plot", "Create a ParaView visualization file with the solution", plot);
     try { cmd.getValues(argc,argv); } catch (int rv) { return rv; }
 
@@ -53,26 +53,41 @@ int main(int argc, char* argv[])
                     |-----------|
     */
 
-    mpBspline.addPatch( gsNurbsCreator<>::BSplineSquare(1) ); //
-    mpBspline = mpBspline.uniformSplit(); // patches 0 to 3
-    mpBspline.addPatch( gsNurbsCreator<>::BSplineSquare(1) ); // patch 4
-    mpBspline.addPatch( gsNurbsCreator<>::BSplineSquare(1) ); // patch 5
-    mpBspline.embed(3);
+    if (geometry==0)
+    {
+        mpBspline.addPatch( gsNurbsCreator<>::BSplineSquare(1) ); //
+        mpBspline = mpBspline.uniformSplit(); // patches 0 to 3
 
-    gsMatrix<> ones;
-    ones = gsMatrix<>::Ones(mpBspline.patch(4).coefs().rows(),1); // patch 4
+        mpBspline.embed(3);
+        mpBspline.addAutoBoundaries();
+    }
+    else if (geometry==1)
+    {
+        mpBspline.addPatch( gsNurbsCreator<>::BSplineSquare(1) ); //
+        mpBspline = mpBspline.uniformSplit(); // patches 0 to 3
+        mpBspline.addPatch( gsNurbsCreator<>::BSplineSquare(1) ); // patch 4
+        mpBspline.addPatch( gsNurbsCreator<>::BSplineSquare(1) ); // patch 5
+        mpBspline.embed(3);
 
-    // translate second patch to the right
-    mpBspline.patch(4).coefs().col(1) += ones; // patch 4
+        gsMatrix<> ones;
+        ones = gsMatrix<>::Ones(mpBspline.patch(4).coefs().rows(),1); // patch 4
 
-    ones = gsMatrix<>::Ones(mpBspline.patch(5).coefs().rows(),1); // patch 5
-    mpBspline.patch(5).coefs().col(0) -= ones; // patch 5
-    mpBspline.patch(5).coefs().col(1) += ones; // patch 5
+        // translate second patch to the right
+        mpBspline.patch(4).coefs().col(1) += ones; // patch 4
+
+        ones = gsMatrix<>::Ones(mpBspline.patch(5).coefs().rows(),1); // patch 5
+        mpBspline.patch(5).coefs().col(0) -= ones; // patch 5
+        mpBspline.patch(5).coefs().col(1) += ones; // patch 5
 
 
-    mpBspline.addInterface(1,boundary::side::north,4,boundary::side::south);
-    mpBspline.addInterface(3,boundary::side::north,4,boundary::side::south);
-    mpBspline.addInterface(4,boundary::side::west,5,boundary::side::east);
+        mpBspline.addInterface(1,boundary::side::north,4,boundary::side::south);
+        mpBspline.addInterface(3,boundary::side::north,4,boundary::side::south);
+        mpBspline.addInterface(4,boundary::side::west,5,boundary::side::east);
+
+        mpBspline.addAutoBoundaries();
+    }
+    else
+        GISMO_ERROR("Geometry with index "<<geometry<<" unknown.");
 
     mpBspline.addAutoBoundaries();
     gsDebugVar(mpBspline.nInterfaces());
@@ -167,81 +182,158 @@ int main(int argc, char* argv[])
 
 
     gsMatrix<index_t> indices1, indices2, corners;
-    size_t n;
     index_t p1, p2;
     boxSide s1, s2;
+    gsBasis<> * basis1;
+    gsBasis<> * basis2;
+    boundaryInterface iface;
     corners.resize(2,1);
-    for(gsBoxTopology::const_iiterator it = mpBspline.iBegin(); it!= mpBspline.iEnd(); it++)
+
+    // Interior interfaces
+    for(gsBoxTopology::const_iiterator iit = mpBspline.iBegin(); iit!= mpBspline.iEnd(); iit++)
     {
-        // use getContainedCorners(...) in gsBoundary.h?
+        // MATCH THE INTERIOR COEFFICIENTS
+        p1 = iit->first().patch;
+        p2 = iit->second().patch;
+
+        s1 = iit->first().side();
+        s2 = iit->second().side();
+
+        gsInfo<<"Interface between patch "<<p1<<" (side "<<s1<<") and patch "<<p2<<" (side "<<s2<<")\n";
+
+        basis1 = &mpBspline.basis(p1);
+        basis2 = &mpBspline.basis(p2);
+
+        mpBspline.getInterface(patchSide(p1,s1),iface);
+        gsDebugVar(iface);
+        basis1->matchWith(iface,*basis2,indices1,indices2);
+
+        gsInfo<<"\tInterface indices (matching):\n";
+        gsInfo<<"\t\tBoundary 1: "<<indices1.transpose()<<"\n";
+        gsInfo<<"\t\tBoundary 2: "<<indices2.transpose()<<"\n";
+
+        gsInfo<<"\tNext-to-interface indices (matching):\n";
+        indices1 = basis1->boundaryOffset(s1,1);
+        indices2 = basis2->boundaryOffset(s2,1);
+        gsInfo<<"\t\tBoundary 1: "<<indices1.transpose()<<"\n";
+        gsInfo<<"\t\tBoundary 2: "<<indices2.transpose()<<"\n";
 
 
+        // const index_t b1 = ;
+        // const index_t b2 = s2.direction();
 
-        // MATCH THE INTERIOR COEFFICNETS
-            p1 = it->first().patch;
-            p2 = it->second().patch;
+        // Flip the index vector if the directions of the indices do not match
+            // gsDebugVar(iit->dirOrientation());
+            // gsDebugVar(iit->dirMap());
+        gsVector<bool> dirOr = iit->dirOrientation();
+        for (short_t k = 0; k<mpBspline.dim(); ++k )
+        {
+            if ( k == s1.direction() ) // skip ?
+                continue;
 
-            s1 = it->first().side();
-            s2 = it->second().side();
-
-            gsDebug<<"Interface between patch "<<p1<<" (side "<<s1<<") and patch "<<p2<<" (side "<<s2<<")\n";
-
-            //>>>>>>
-            // COUPLE/MATCH COEFFICIENTS of INTERIOR ONLY
-
-            // get the indices of the basis functions adjacent to the interface
-
-
-            // gsDebugVar(it->first().patch);
-            // gsDebugVar();
-            // gsDebugVar(it->second());
-
-            // indices = bases.basis(i).boundary(it);
-            // GISMO_ASSERT(indices.cols()==1,"Columns of indices should be 1. Someting went wrong");
-            // n = indices.size();
-            // corners.topRows(1) = indices.topRows(1);
-            // corners.bottomRows(1) = indices.bottomRows(1);
-            // indices = indices.block(1,0,n-2,1);
-
-            //<<<<<<
-
-        // FIND INTERIOR VERTICES
-            std::vector<boxCorner> corners;
-            std::vector<patchCorner> cornerList;
-            patchCorner corner;
-
-            s1.getContainedCorners(mpBspline.dim(),corners);
-            for (index_t k = 0; k != corners.size(); k++)
+            if ( ! dirOr[k] ) // flip ?
             {
-                corner = static_cast<patchCorner &>(corners[k]);
-                bool isCycle = mpBspline.getCornerList(corner,cornerList);
-                gsInfo<<corners[k]<<" (valence = "<<cornerList.size()<<")\n";
+                gsDebug<<"\t\tReversed direction\n";
+                indices1.reverse();
             }
+        }
 
-            gsInfo<<"----\n";
+        // COUPLE/MATCH COEFFICIENTS of INTERIOR ONLY
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            size_t n = indices1.size();
+            // get the indices of the basis functions adjacent to the interface
+            GISMO_ASSERT(indices1.cols()==1,"Columns of indices should be 1. Someting went wrong");
+            indices1 = indices1.block(1,0,n-2,1);
+            indices2 = indices2.block(1,0,n-2,1);
+            // corners.topRows(1) = indices1.topRows(1);
+            // corners.bottomRows(1) = indices1.bottomRows(1);
+        //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+        // TREATMENT FOR THE BOUNDARY CONTROLPOINTS/BASISFUNCTIONS HERE!
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        GISMO_ASSERT(indices1.rows()==indices2.rows(),"Indices do not have the same size. Someting went wrong?");
+        // for (index_t k=0; k!= indices1.rows(); k++)
+        // {
+
+        // }
+        //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     }
-
-    for(gsBoxTopology::const_biterator it = mpBspline.bBegin(); it!= mpBspline.bEnd(); it++)
+    // Boundary interfaces
+    for(gsBoxTopology::const_biterator bit = mpBspline.bBegin(); bit!= mpBspline.bEnd(); bit++)
     {
-            p1 = it->patch;
-            s1 = it->side();
+            p1 = bit->patch;
+            s1 = bit->side();
             gsDebug<<"Boundary of patch "<<p1<<" (side "<<s1<<")\n";
 
-            std::vector<boxCorner> corners;
+            basis1 = &mpBspline.basis(p1);
+            indices1 = basis1->boundary(s1);
+            gsInfo<<"\tBoundary indices: "<<indices1.transpose()<<"\n";
+            indices1 = basis1->boundaryOffset(s1,1);
+            gsInfo<<"\tNext-to-boundary indices: "<<indices1.transpose()<<"\n";
+
+            // GET THE INDICES OF THE NON-VERTEX BASIS FUNCTIONS
+            // QUESTION: THIS ONLY WORKS IF THE CORNERS ARE THE ENDS OF THE INDEX VECTORS. IS THAT THE CASE????
+            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            GISMO_ASSERT(indices1.cols()==1,"Columns of indices should be 1. Someting went wrong");
+            n = indices1.size();
+            corners.topRows(1) = indices1.topRows(1);
+            corners.bottomRows(1) = indices1.bottomRows(1);
+            indices1 = indices1.block(1,0,n-2,1);
+            //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+            // TREATMENT FOR THE BOUNDARY CONTROLPOINTS/BASISFUNCTIONS HERE!
+            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            // for (index_t k=0; k!= indices1.rows(); k++)
+            // {
+
+            // }
+            //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    }
+
+    // Vertices
+    std::vector<bool> marked;
+    marked.resize(4*mpBspline.nPatches()); // [p1c1, p1c2,p1c3,p1c4,p2c1,...,p2c4,...,pNc1,...pNc4]
+    std::fill(marked.begin(), marked.end(), false);
+
+    index_t cIdx;
+    boxCorner corner;
+    for(index_t p=0; p!=mpBspline.nPatches(); p++) // for all patches
+    {
+        gsInfo<<"Patch "<<p<<"\n";
+        for (index_t c=1; c!=5; c++) // for all corners [corners run from 1 up to 4]
+        {
+            if (marked[4*p+c-1])
+                continue;
+
+            corner = boxCorner(c);
+            gsInfo<<"\tcorner "<<corner<<"\n";
+
+            basis1 = &mpBspline.basis(p);
+            cIdx = basis1->functionAtCorner(corner);
+            gsInfo<<"\t\tfunction index "<<cIdx<<"\n";
+
+
             std::vector<patchCorner> cornerList;
-            patchCorner corner;
-            s1.getContainedCorners(mpBspline.dim(),corners);
-            for (index_t k = 0; k != corners.size(); k++)
-            {
-                corner = static_cast<patchCorner &>(corners[k]);
-                bool isCycle = mpBspline.getCornerList(corner,cornerList);
-                gsInfo<<corners[k]<<" (valence = "<<cornerList.size()<<") --> surroundingCorners";
-                for (std::vector<patchCorner>::iterator it = cornerList.begin(); it != cornerList.end(); it++)
-                {
-                    gsInfo<<" patch "<<it->patch<<"; ";
-                }
-                gsInfo<<"\n";
-            }
+            patchCorner pcorner = patchCorner(p,corner);
+            bool isCycle = mpBspline.getCornerList(pcorner,cornerList);
+
+            gsInfo<<"\t\tvalence "<<cornerList.size()<<"\n";
+            gsInfo<<"\t\ttype:"<<( isCycle ? " interior vertex " : " boundary vertex " )<<"\n";
+
+
+            // TREATMENT FOR THE SURROUNDING VERTICES HERE!
+            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            // for (std::vector<patchCorner>::iterator cit = cornerList.begin(); cit != cornerList.end(); cit++)
+            // {
+                // gsInfo<<" patch "<<*cit<<"; ";
+            // }
+            //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+            // we can use this to store marked corners later
+            marked[4*p + c - 1] = true; //corners run from 1 up to 4
+        }
     }
 
     // ----------------------------------------------------------------------
